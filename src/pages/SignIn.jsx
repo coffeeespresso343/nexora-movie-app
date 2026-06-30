b60cd20aa793903d91e51c7c467cce9f7bbf6d4b
Coffee Espresso <coffeeespresso304@gmail.com>
2026-06-30 21:46:26 +0630

    Disabled OAuth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -96,8 +96,9 @@ const SignIn = () => {
 
         <button
           type="button"
+          title="Coming Soon..."
           onClick={loginWithGoogle}
-          className="mt-6 flex w-full items-center justify-center gap-2 rounded-full border border-white/15 px-4 py-2.5 font-medium transition hover:bg-white/10 cursor-pointer"
+          className="cursor-not-allowed mt-6 flex w-full items-center justify-center gap-2 rounded-full border border-white/15 px-4 py-2.5 font-medium transition hover:bg-white/10 "
         >
           <svg width="18" height="18" viewBox="0 0 24 24">
             <path
